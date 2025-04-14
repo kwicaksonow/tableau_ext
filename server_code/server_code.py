0037d2b557e857efbda097bdf44e5bd723b75a42
kukuh.wicaksono@tripakarta.co.id <kukuh.wicaksono@tripakarta.co.id>
2025-04-14 03:29:48 +0000

    Edited client_code, settings and server_code

--- server_code/server_code.py
+++ server_code/server_code.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 
 GOOGLE_API_KEY = anvil.secrets.get_secret("GEMINI_API")
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel(model_name='gemini-1.5-flash', system_instruction=[
+model = genai.GenerativeModel(model_name='gemini-2.0-flash', system_instruction=[
         "You are an expert analyst and know everything about data analysis",
         "You can interpret data in any form whether it's a single data point or a list of data with keys"
         "You are on a mission to provide the best data analysis report when asked",
